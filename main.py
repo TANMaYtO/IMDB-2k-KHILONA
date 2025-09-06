8e9bd459f539374ef0dc445c1bdff78219f77350
Tanmay <tomartanmay8@gmail.com>
2025-09-06 11:18:25 +0530

    got the best lr

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ category = [category for words, category in data]
 split_idx = int(len(texts)*0.8)
 x_train,x_test, y_train, y_test = texts[:split_idx], texts[split_idx:], category[:split_idx], category[split_idx:]
 
-vec = TfidfVectorizer(max_features=10000)
+vec = TfidfVectorizer(max_features=5000)
 x_train_vec = vec.fit_transform(x_train)
 x_test_vec = vec.transform(x_test)
 
